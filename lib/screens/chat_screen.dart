45429eca9d49cdc1af97fc3404f2b0ddcb0e8d28
NadaAlaa1 <nalaa839@gmail.com>
2024-12-21 20:35:17 +0200

    triggering chat cubit

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,105 +1,98 @@
 import 'package:chat_app/constants.dart';
 import 'package:chat_app/models/message.dart';
+import 'package:chat_app/screens/cubits/chat_cubit/chat_cubit.dart';
 import 'package:chat_app/widgets/chat_bubble.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ChatScreen extends StatelessWidget {
   ChatScreen({super.key});
   static String id = 'ChatScreen';
 
   final _controller = ScrollController();
+  List<Message> messagesList = [];
 
-  CollectionReference messages =
-      FirebaseFirestore.instance.collection(kMessagesCollections);
   TextEditingController controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    var email = ModalRoute.of(context)!.settings.arguments;
+    var email = ModalRoute.of(context)?.settings.arguments as String? ?? '';
+
     final size = MediaQuery.of(context).size;
 
-    return StreamBuilder<QuerySnapshot>(
-      stream: messages.orderBy(kCreatedAt, descending: true).snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          List<Message> messagesList = [];
-          for (var doc in snapshot.data!.docs) {
-            messagesList.add(Message.fromJson(doc));
-          }
-
-          return Scaffold(
-            appBar: AppBar(
-              automaticallyImplyLeading: false,
-              backgroundColor: kPrimaryColor,
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    kLogo,
-                    height: size.height * 0.05,
-                  ),
-                  const SizedBox(width: 8),
-                  const Text('Chat'),
-                ],
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: kPrimaryColor,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              kLogo,
+              height: size.height * 0.05,
+            ),
+            const SizedBox(width: 8),
+            const Text(
+              'Chat',
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: BlocBuilder<ChatCubit, ChatState>(
+              builder: (context, state) {
+                var messagesList =
+                    BlocProvider.of<ChatCubit>(context).messagesList;
+                return ListView.builder(
+                  reverse: true,
+                  controller: _controller,
+                  itemCount: messagesList.length,
+                  itemBuilder: (context, index) {
+                    return messagesList[index].id == email
+                        ? ChatBubble(message: messagesList[index])
+                        : ChatBubbleForFriend(message: messagesList[index]);
+                  },
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(size.width * 0.04),
+            child: TextField(
+              controller: controller,
+              onSubmitted: (data) {
+                BlocProvider.of<ChatCubit>(context)
+                    .sendMessage(message: data, email: email);
+                controller.clear();
+                _controller.animateTo(
+                  0,
+                  duration: const Duration(seconds: 1),
+                  curve: Curves.easeIn,
+                );
+              },
+              decoration: InputDecoration(
+                hintText: 'Message',
+                suffixIcon: Icon(
+                  Icons.send,
+                  color: kPrimaryColor,
+                  size: size.width * 0.07,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(size.width * 0.04),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(size.width * 0.04),
+                  borderSide: const BorderSide(color: kPrimaryColor),
+                ),
               ),
-              centerTitle: true,
             ),
-            body: Column(
-              children: [
-                Expanded(
-                  child: ListView.builder(
-                    reverse: true,
-                    controller: _controller,
-                    itemCount: messagesList.length,
-                    itemBuilder: (context, index) {
-                      return messagesList[index].id == email
-                          ? ChatBubble(message: messagesList[index])
-                          : ChatBubbleForFriend(message: messagesList[index]);
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(size.width * 0.04),
-                  child: TextField(
-                    controller: controller,
-                    onSubmitted: (data) {
-                      messages.add({
-                        kMessage: data,
-                        kCreatedAt: DateTime.now(),
-                        'id': email,
-                      });
-                      controller.clear();
-                      _controller.animateTo(
-                        0,
-                        duration: const Duration(seconds: 1),
-                        curve: Curves.easeIn,
-                      );
-                    },
-                    decoration: InputDecoration(
-                      hintText: 'Message',
-                      suffixIcon: Icon(
-                        Icons.send,
-                        color: kPrimaryColor,
-                        size: size.width * 0.07,
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(size.width * 0.04),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(size.width * 0.04),
-                        borderSide: const BorderSide(color: kPrimaryColor),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          );
-        } else {
-          return const Center(child: CircularProgressIndicator());
-        }
-      },
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/screens/cubits/chat_cubit/chat_cubit.dart
+++ lib/screens/cubits/chat_cubit/chat_cubit.dart
@@ -1,7 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:chat_app/constants.dart';
+import 'package:chat_app/models/message.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/foundation.dart';
 
 part 'chat_state.dart';
 
@@ -11,17 +12,29 @@ class ChatCubit extends Cubit<ChatState> {
   CollectionReference messages =
       FirebaseFirestore.instance.collection(kMessagesCollections);
 
+  List<Message> messagesList = [];
+
   void sendMessage({required String message, required String email}) {
-    messages.add({
-      kMessage: message,
-      kCreatedAt: DateTime.now(),
-      'id': email,
-    });
+    try {
+      messages.add({
+        kMessage: message,
+        kCreatedAt: DateTime.now(),
+        'id': email,
+      });
+    } on Exception catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
   }
 
-  void getMessages(){
-    messages.orderBy(kCreatedAt, descending: true).snapshots().listen((event){
-      emit(ChatSuccess());
+  void getMessages() {
+    messages.orderBy(kCreatedAt, descending: true).snapshots().listen((event) {
+      messagesList.clear();
+      for (var doc in event.docs) {
+        messagesList.add(Message.fromJson(doc));
+      }
+      emit(ChatSuccess(messages: messagesList));
     });
   }
 }
